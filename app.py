97f76fddd71f6c4f94df0f675f286cc885d57f44
MMorokk <51864605+MMorokk@users.noreply.github.com>
2025-06-01 15:47:54 +0200

    feat: Loading env file and using it.
    Added dependency: dotenv
    Injecting WEBSITE_TITLE to all of the jinja templates, if WEBSITE_TITLE is unset default to "GetPet"

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+# Make environment variables available to all templates
+@app.context_processor
+def inject_env_vars():
+    return {
+        'website_title': os.getenv('WEBSITE_TITLE', 'GetPet'),
+    }
 
 @app.route("/")
 def hello_world():
