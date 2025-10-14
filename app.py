ad5645f515725da21c1cafeb26083708e50836b9
Morok <51864605+MMorokk@users.noreply.github.com>
2025-10-14 10:46:18 +0200

    feat!: Parsing info from markdown

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-import pet_loader
+from loaders import *
 from dotenv import load_dotenv
-from markupsafe import escape
+# from markupsafe import escape
+from markdown import markdown
 from flask import (
     Flask,
     render_template,
@@ -10,7 +11,8 @@ from flask import (
 )
 from flask_minify import minify
 
-load_dotenv()
+# load_dotenv()
+info = load_toml(os.path.join(os.path.dirname(__file__), 'info.toml'))
 
 app = Flask(__name__)
 
@@ -18,24 +20,29 @@ app = Flask(__name__)
 # speed up loading times and save bandwith
 minify(app=app, html=True, js=True, cssless=True)
 
-# Make environment variables available to all templates
-@app.context_processor
-def inject_env_vars():
-    return {
-        "website_title": os.getenv("WEBSITE_TITLE", "GetPet"),
-        "please_contact": os.getenv("PLEASE_CONTACT", False),
-        "contact_address": os.getenv("CONTACT_ADDRESS", "Chicago, US"),
-        "contact_phone": os.getenv("CONTACT_PHONE", "+380999123921"),
-        "contact_email": os.getenv("CONTACT_EMAIL", "mail@example.com"),
-    }
+# # Make environment variables available to all templates
+# @app.context_processor
+# def inject_info():
+#     return {
+#         "website_title": os.getenv("WEBSITE_TITLE", "GetPet"),
+#         "please_contact": os.getenv("PLEASE_CONTACT", False),
+#         "contact_address": os.getenv("CONTACT_ADDRESS", "Chicago, US"),
+#         "contact_phone": os.getenv("CONTACT_PHONE", "+380999123921"),
+#         "contact_email": os.getenv("CONTACT_EMAIL", "mail@example.com"),
+#     }
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", 
+                           header = info["home"]["header"], 
+                           text = markdown(info["home"]["text"], extensions=['nl2br']),
+                           button = info["home"]["button"])
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",
+                           header = info["about"]["header"], 
+                           text = markdown(info["about"]["text"], extensions=['nl2br']))
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
